85ea098f56f5c51bdb23d06de97aaebc8fa45b1f
eunjeong <tanvely0409@gmail.com>
2019-08-14 15:14:09 +0900

    home navigation 수정

--- src/main/java/com/sample/camping/controller/NoticeController.java
+++ src/main/java/com/sample/camping/controller/NoticeController.java
@@ -1,14 +1,12 @@
 package com.sample.camping.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sample.camping.service.NoticeService;
